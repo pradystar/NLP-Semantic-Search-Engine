108f02968151f40c85ae4c4db806a8e25e730c8d
pradystar <pradystar@gmail.com>
2017-12-06 22:11:34 -0600

    added search options

--- search.py
+++ search.py
@@ -15,62 +15,84 @@ from indexer import get_dependency_relations
 
 import pysolr
 
-def search(query, instance_url='http://localhost:8983/solr/collection_1/'):
+def search(query, method, instance_url):
     '''
     search solr index using user query
     '''
-    stemmer = PorterStemmer()
-    tokens = word_tokenize(query)
-    tagged_tokens = pos_tag(tokens)
-    synonyms, hypernyms, hyponyms, meronyms, holonyms = get_semantic_features(tagged_tokens, tokens)
-    lemmas = get_lemmatized_line(tagged_tokens)
-    head_words = get_dependency_relations(lemmas)
-    stem_line = [stemmer.stem(t) for t in tokens]
-    stems = ' '.join(stem_line)
-    tagged_list = [tuple2str(t) for t in tagged_tokens]
-
-    pos_tag_data = ' '.join(tagged_list)
-    # print('text:' + query, ',pos_tag:'+ pos_tag_data, ',lemmas:'+lemmas, ',stems:'+stems,
-    # ',synonyms:'+synonyms, ',hypernyms:'+hypernyms, ',hyponyms:'+hyponyms,'holonymns:'+holonyms)
-    solr = pysolr.Solr(instance_url)
-    query = '&'.join(query.split())
-    pos_tag_data = '&'.join(pos_tag_data.split())
-    lemmas = '&'.join(lemmas.split())
-    stems = '&'.join(stems.split())
-    synonyms = '&'.join(synonyms.split())
-    hypernyms = '&'.join(hypernyms.split())
-    hyponyms = '&'.join(hyponyms.split())
-    holonyms = '&'.join(holonyms.split())
-    meronyms = '&'.join(meronyms.split())
     q_list = []
-    if query:
-        q_list.append('text:' + query + '^0.5')
-    if pos_tag_data:
-        q_list.append('pos_tag:' + pos_tag_data + '^0.02')
-    if lemmas:
-        q_list.append('lemmas:' + lemmas + '^3')
-    # if stems:
-    #     q_list.append('stems:' + stems + '^1.5')
-    if synonyms:
-        q_list.append('synonyms:' + synonyms + '^3')
-    if hypernyms:
-        q_list.append('hypernyms:' + hypernyms + '^4')
-    if hyponyms:
-        q_list.append('hyponyms:' + hyponyms + '^4')
-    if 
-    # if meronyms:
-    #     q_list.append('meronyms:' + meronyms + '^1.4')
-    # if holonyms:
-    #     q_list.append('holonymns:' + holonyms + '^1.4')
-    # print(','.join(q_list))
+    solr = pysolr.Solr(instance_url)
+    tokens = word_tokenize(query)
+    if method == 1:
+        q_list.append('text:' + ' '.join(tokens))
+    else:
+        stemmer = PorterStemmer()
+        tagged_tokens = pos_tag(tokens)
+        synonyms, hypernyms, hyponyms, meronyms, holonyms = get_semantic_features(
+            tagged_tokens, tokens)
+        lemmas = get_lemmatized_line(tagged_tokens)
+        head_words = get_dependency_relations(lemmas, True)
+        stem_line = [stemmer.stem(t) for t in tokens]
+        tagged_list = [tuple2str(t) for t in tagged_tokens]
+
+        pos_tag_data = '&'.join(tagged_list)
+        lemmas = '&'.join(lemmas.split())
+        stems = '&'.join(stem_line)
+        synonyms = '&'.join(synonyms.split())
+        hypernyms = '&'.join(hypernyms.split())
+        hyponyms = '&'.join(hyponyms.split())
+        holonyms = '&'.join(holonyms.split())
+        meronyms = '&'.join(meronyms.split())
+        head_words = '&'.join(head_words.split())
+        if method == 2:
+            if tokens:
+                q_list.append('text:' + '&'.join(tokens))
+            if pos_tag_data:
+                q_list.append('pos_tag:' + pos_tag_data)
+            if lemmas:
+                q_list.append('lemmas:' + lemmas)
+            if stems:
+                q_list.append('stems:' + stems)
+            if synonyms:
+                q_list.append('synonyms:' + synonyms)
+            if hypernyms:
+                q_list.append('hypernyms:' + hypernyms)
+            if hyponyms:
+                q_list.append('hyponyms:' + hyponyms)
+            if meronyms:
+                q_list.append('meronyms:' + meronyms)
+            if holonyms:
+                q_list.append('holonymns:' + holonyms)
+            if head_words:
+                q_list.append('head_word:' + head_words)
+        if method == 3:
+            if tokens:
+                q_list.append('text:' + '&'.join(tokens) + '^0.5')
+            if pos_tag_data:
+                q_list.append('pos_tag:' + pos_tag_data + '^0.02')
+            if lemmas:
+                q_list.append('lemmas:' + lemmas + '^3')
+            # if stems:
+            #     q_list.append('stems:' + stems + '^1.5')
+            if synonyms:
+                q_list.append('synonyms:' + synonyms + '^3')
+            if hypernyms:
+                q_list.append('hypernyms:' + hypernyms + '^4')
+            # if hyponyms:
+            #     q_list.append('hyponyms:' + hyponyms + '^4')
+            if head_words:
+                q_list.append('head_word:' + head_words + '^3')
+            # if meronyms:
+            #     q_list.append('meronyms:' + meronyms + '^1.4')
+            # if holonyms:
+            #     q_list.append('holonymns:' + holonyms + '^1.4')
     q_string = ', '.join(q_list)
-    print(q_string)
+    print('The Solr query is q=%s, fl=\'id,text\'' % (q_string))
     result = solr.search(q=q_string, fl='id,text')
     # for r in json.dumps(result.docs):
     #     print(r)
     for r in result:
         print(r['id'])
-        print(r['text'])
+        print(' '.join(r['text']))
 
 def main():
     '''
@@ -80,9 +102,19 @@ def main():
     # line = "stop illegal fishing"
     # line = "spread hiv aids"
     # line = "the School of the Air"
-    line = 'where is rice grown'
-    line = 'largest dinosaur fossil ever found in an excavation site'
-    search(line)
+    # line = 'where is rice grown'
+    # line = 'largest dinosaur fossil ever found in an excavation site'
+    method = int(sys.argv[1])
+    instance = 'http://localhost:8983/solr/collection_1/'
+    if method == 1:
+        print('Performing naive search...')
+        instance = 'http://localhost:8983/solr/collection_0/'
+    elif method == 2:
+        print('Performing deep NLP pipeline search...')
+    else:
+        print('Performing improved deep NLP pipeline search...')
+    line = input('Enter search query: ')
+    search(line, method, instance)
 
 if __name__ == '__main__':
     main()
